abfb1b62757359d8bddc0ed948e549ec0ef80f89
Allen D. Householder <adh@cert.org>
2019-04-12 14:47:40 -0400

    format with black

--- vxref/src/validate_vxref.py
+++ vxref/src/validate_vxref.py
@@ -1,8 +1,8 @@
-'''
+"""
 Created on May 17, 2016
 
 @author: adh
-'''
+"""
 
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
@@ -10,26 +10,29 @@ import json
 
 
 def load_doc(doc_path):
-    with open(doc_path, 'r') as f:
+    with open(doc_path, "r") as f:
         doc = json.load(f)
     return doc
 
-def validate_vxref(doc,schema):
+
+def validate_vxref(doc, schema):
     try:
         # validate doc against schema
         validate(doc, schema)
     except ValidationError as e:
         return e
 
-def process_doc(doc_path,schema):
+
+def process_doc(doc_path, schema):
     print("Validating {}".format(doc_path))
     doc = load_doc(doc_path)
 
-    result = validate_vxref(doc,schema)
+    result = validate_vxref(doc, schema)
     if result is None:
         print("... passed.")
     else:
-        print result
+        print(result)
+
 
 def main():
     import glob
@@ -38,9 +41,10 @@ def main():
     schema_path = "../schema/vxref_schema_03.json"
     schema = load_doc(schema_path)
 
-    data_dir = '../data/v03'
-    for doc_path in glob.glob(os.path.join(data_dir,'*.json')):
-        process_doc(doc_path,schema)
+    data_dir = "../data/v03"
+    for doc_path in glob.glob(os.path.join(data_dir, "*.json")):
+        process_doc(doc_path, schema)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
